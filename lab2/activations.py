4714e8d2b474ccd27d5d64e2674eba35e48b02f1
Alexandru Robert <robertalexandru2712@gmail.com>
2021-12-28 11:32:21 +0200

    Add softmax function.

--- lab2/activations.py
+++ lab2/activations.py
@@ -4,6 +4,7 @@ def softmax(x, t=1):
     """"
     Applies the softmax temperature on the input x, using the temperature t
     """
-    # TODO your code here
-    # end TODO your code here
-    return None
+    exp_x = np.exp(x / t)
+    sum_exp_x = np.sum(exp_x)
+    sm_x = exp_x/sum_exp_x
+    return sm_x
