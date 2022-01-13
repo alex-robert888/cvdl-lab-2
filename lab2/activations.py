92dafd62416fd64f92e42e0acddc6d1e2383360a
Alexandru Robert <robertalexandru2712@gmail.com>
2022-01-13 10:17:58 +0200

    Finish laboratory 2.

--- lab2/activations.py
+++ lab2/activations.py
@@ -4,7 +4,8 @@ def softmax(x, t=1):
     """"
     Applies the softmax temperature on the input x, using the temperature t
     """
+    x = x - np.max(x)
     exp_x = np.exp(x / t)
     sum_exp_x = np.sum(exp_x)
-    sm_x = exp_x/sum_exp_x
+    sm_x = exp_x / sum_exp_x
     return sm_x
--- lab2/softmax.py
+++ lab2/softmax.py
@@ -1,9 +1,8 @@
+from os import X_OK
 import numpy as np
 from numpy.core.fromnumeric import size
-from lab2.activations import softmax
+from activations import softmax
 from functools import reduce
-
-from lab2.train import X_train
 
 class SoftmaxClassifier:
     def __init__(self, input_shape, num_classes):
@@ -17,11 +16,8 @@ class SoftmaxClassifier:
         # initialize the weight matrix (remember the bias trick) with small random variables
         # you might find np.random.randn userful here *0.001
         weights_height = self.__num_classes
-        weights_width  = 1 + self.__input_shape[0] * self.__input_shape[1]
+        weights_width  = self.__input_shape
         self.__weights = 0.001 * np.random.randn(weights_height, weights_width)
-        
-        # Initialize the bias column with zeros
-        self.__weights[:, -1] = np.zeros(weights_height)
 
     def predict_proba(self, X: np.ndarray) -> np.ndarray:
         scores = None
@@ -30,17 +26,17 @@ class SoftmaxClassifier:
         # remember about the bias t rick!
         # 1. apply the softmax function on the scores
         # 2. returned the normalized scores
-        scores = self.__compute_output()
+        scores = self.__compute_output(X)
         return scores
 
-    def predict(self, X: np.ndarray) -> int:
-        label = None
+    def predict(self, X_batch: np.ndarray) -> int:
+        labels = None
         # TODO your code here
         # 0. compute the dot product between the weight matrix and the input X as the scores
         # 1. compute the prediction by taking the argmax of the class scores
-        output = self.__compute_output(with_softmax=False)
-        label  = output.argmax()
-        return label
+        outputs = self.__compute_output(X_batch, with_softmax=False)
+        labels = np.argmax(outputs, axis=0)
+        return labels
 
     def fit(self, X_train: np.ndarray, y_train: np.ndarray,
             **kwargs) -> dict:
@@ -51,20 +47,17 @@ class SoftmaxClassifier:
         reg_strength = kwargs['reg_strength'] if 'reg_strength' in kwargs else 1e3
         steps = kwargs['steps'] if 'steps' in kwargs else 100
         lr = kwargs['lr'] if 'lr' in kwargs else 1e-3
-        print(bs, reg_strength, steps, lr)
 
         # run mini-batch gradient descent
         for iteration in range(0, steps):
-            print(f"mini-batch no. {iteration}")
-
             # TODO your code here
             # sample a batch of images from the training set
             # you might find np.random.choice useful
-            indices = np.random.choice(len(X_train), bs)
+            indices = np.random.choice(range(0, np.shape(X_train)[0]), size=bs)
             X_batch, y_batch = X_train[indices], y_train[indices]
             # compute the loss and dW
             dW = self.__compute_gradient(X_batch, y_batch)
-            loss = self.__compute_loss(X_batch, y_batch, bs, reg_strength)
+            loss = self.__compute_loss(X_batch, y_batch, reg_strength)
             # end TODO your code here
             # perform a parameter update
             self.__weights -= lr * dW
@@ -79,6 +72,9 @@ class SoftmaxClassifier:
         # TODO your code here
         # 0. ignore the bias term
         # 1. reshape the weights to (*image_shape, num_classes)
+
+        W = self.__weights[:, :-1]
+        W = np.reshape(W, (self.__num_classes, *img_shape))
         return W
 
     def load(self, path: str) -> bool:
@@ -105,17 +101,18 @@ class SoftmaxClassifier:
 
     def __compute_output(self, X: np.ndarray, with_softmax: bool=True):
         # Compute the output column
-        output = np.dot(X, self.__weights)
+        output = np.dot(self.__weights, np.transpose(X))
         output = softmax(output) if with_softmax else output 
         return output
 
     def __compute_gradient(self, X_batch: np.ndarray, y_batch: np.ndarray):
         # Compute the gradient of the mini-batch
         X_batch_transposed                        = X_batch.transpose()
-        one_hot                                   = np.zeros(y_batch.size, (self.__num_classes))
+        one_hot                                   = np.zeros((y_batch.size, y_batch.max() + 1))
         one_hot[np.arange(y_batch.size), y_batch] = 1
-        CT                                        = softmax(X_batch * self.__weights) - one_hot
-        return np.dot(X_batch_transposed, CT)
+        CT                                        = softmax(np.dot(X_batch, np.transpose(self.__weights)))
+        CT                                       -= one_hot
+        return np.transpose(np.dot(X_batch_transposed, CT))
 
     def __compute_loss(self, X_batch: np.ndarray, y_batch: np.ndarray, regularization_strength: float):
         average_loss = self.__compute_average_loss(X_batch, y_batch)
@@ -123,15 +120,14 @@ class SoftmaxClassifier:
         return average_loss + regularization_term 
         
     def __compute_average_loss(self, X_batch: np.ndarray, y_batch: np.ndarray):
-        losses = np.vectorize(self.__compute_image_loss)(np.column_stack(X_batch, y_batch))
-        return 1 / X_batch.size() * losses.sum()
+        losses = np.array([self.__compute_image_loss(X, y) for X, y in zip(X_batch, y_batch)])
+        return 1 / X_batch.size * losses.sum()
 
-    def __compute_image_loss(self, X_and_y):
-        X, y = X_and_y
+    def __compute_image_loss(self, X, y):
         output = self.__compute_output(X)
         return -1 * output[y] + np.log(np.sum(np.exp(output)))
 
     def __compute_regularization_term(self, regularization_strength):
-        sum_of_weights_squares = sum(self.__weights ** 2)
-        return regularization_strength * sum_of_weights_squares
+        sum_of_weights = sum(self.__weights)
+        return regularization_strength * sum_of_weights
 
